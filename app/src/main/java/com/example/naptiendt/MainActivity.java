d675e6ff902e365007a4cd51d04f86b3b98a03f1
Đặng Hoàng Long <tylbam11@gmail.com>
2023-01-29 19:23:38 +0700

    Created buttons, about to set actions for those

--- app/src/main/java/com/example/naptiendt/MainActivity.java
+++ app/src/main/java/com/example/naptiendt/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.naptiendt;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.vision.text.TextRecognizer;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.imageview.ShapeableImageView;
 
 import androidx.camera.core.ImageAnalysis;
 
 public class MainActivity extends AppCompatActivity {
 
+    //UI Views
+    private MaterialButton inputImageButton;
+    private MaterialButton recognizeTextButton;
+    private ShapeableImageView imageView;
+    private EditText recognizedTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize UI Views
+        inputImageButton = findViewById(R.id.inputImageButton);
+        recognizeTextButton = findViewById(R.id.recognizeTextButton);
+        imageView = findViewById(R.id.imageView);
+        recognizedTextView = findViewById(R.id.recognizedTextView);
+
     }
 };
 
-// text recognition app
